4b8cf028778a6066f6fc29795169a63c2ab6969c
Kabore Abdoul wahab <kaboreabwa2020@gmail.com>
2024-11-03 17:34:09 +0000

    Votre message de mise à jour

--- server.js
+++ server.js
@@ -6,17 +6,18 @@ const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
-const User = require('./models/User'); // Assurez-vous que le modèle User est correctement importé
-const Message = require('./models/message'); // Assurez-vous que le modèle Message est également importé
 const http = require('http');
 const socketIo = require('socket.io');
 
+// Modèles
+const User = require('./models/User');
+const Message = require('./models/message');
+const Annonce = require('./models/Annonce'); // Assurez-vous que le chemin du modèle est correct
 
 // Créer une instance de l'application Express
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
-
 
 // Middleware pour analyser le corps des requêtes JSON
 app.use(express.json());
@@ -25,18 +26,16 @@ app.use(bodyParser.json());
 // Middleware pour servir des fichiers statiques
 app.use(express.static('public'));
 
-// Appliquer le middleware CORS sans options (autorise toutes les origines par défaut)
+// Middleware CORS
 app.use(cors({
     origin: 'https://farmsconnect-b084ddb02391.herokuapp.com', // ou '*' pour autoriser toutes les origines
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
 
-
-mongoose.connect('mongodb+srv://kabboss:ka23bo23re23@cluster0.uy2xz.mongodb.net/FarmsConnect?retryWrites=true&w=majority', {
-})
-.then(() => console.log('Connecté à MongoDB...'))
-.catch(err => console.error('Erreur de connexion à MongoDB:', err));
-
+// Connexion à MongoDB
+mongoose.connect('mongodb+srv://kabboss:ka23bo23re23@cluster0.uy2xz.mongodb.net/FarmsConnect?retryWrites=true&w=majority', {})
+    .then(() => console.log('Connecté à MongoDB...'))
+    .catch(err => console.error('Erreur de connexion à MongoDB:', err));
 
 // Configurer le transporteur Nodemailer
 const transporter = nodemailer.createTransport({
@@ -47,24 +46,19 @@ const transporter = nodemailer.createTransport({
     }
 });
 
+// Routes
+
 // Route pour s'inscrire (signup)
 app.post('/api/signup', async (req, res) => {
     const { username, email, contact, password, userType } = req.body;
-
     try {
-        // Vérifie si l'utilisateur existe déjà
         const existingUser = await User.findOne({ username });
         if (existingUser) {
             return res.status(400).send('Cet utilisateur existe déjà.');
         }
-
-        // Hachage du mot de passe avant de l'enregistrer
         const hashedPassword = await bcrypt.hash(password, 10);
-
-        // Créer un nouvel utilisateur avec le mot de passe haché
         const newUser = new User({ username, email, contact, password: hashedPassword, userType });
         await newUser.save();
-
         res.status(201).send('Utilisateur créé avec succès !');
     } catch (error) {
         console.error('Erreur lors de l’inscription :', error);
@@ -75,35 +69,22 @@ app.post('/api/signup', async (req, res) => {
 // Route pour se connecter (login)
 app.post('/api/login', async (req, res) => {
     const { username, email, contact, password } = req.body;
-
     try {
-        // Vérifier que tous les champs sont fournis
         if (!username || !email || !contact || !password) {
             return res.status(400).send('Veuillez fournir le nom d’utilisateur, l\'email, le contact et le mot de passe.');
         }
-
-        // Trouver l'utilisateur en fonction du nom d'utilisateur
         const user = await User.findOne({ username });
-
         if (!user) {
             return res.status(400).send('Nom d’utilisateur incorrect.');
         }
-
-        // Vérifier que l'email et le contact correspondent
         if (user.email !== email || user.contact !== contact) {
             return res.status(400).send('Email ou contact incorrect.');
         }
-
-        // Vérifier que le mot de passe est correct
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) {
             return res.status(400).send('Mot de passe incorrect.');
         }
-
-        // Générer un token JWT
         const token = jwt.sign({ userId: user._id }, 'ka23bo23re23'); // Remplacez par une vraie clé secrète
-
-        // Envoyer la réponse de connexion réussie
         res.status(200).json({ username: user.username, email: user.email, contact: user.contact, token, message: 'Connexion réussie !' });
     } catch (error) {
         console.error('Erreur lors de la connexion :', error);
@@ -114,18 +95,14 @@ app.post('/api/login', async (req, res) => {
 // Route pour passer une commande et envoyer l'email
 app.post('/api/order', async (req, res) => {
     console.log('Données reçues :', req.body);
-
-    // Déconstruction des informations nécessaires de la requête
     const { username, email, contact, price, quantity, weight, Produit: nomproduit } = req.body;
 
-    // Vérification des informations nécessaires
     if (!username || !nomproduit || !email || !contact || !price || !quantity || !weight) {
         console.error('Informations manquantes dans la requête :', req.body);
         return res.status(400).send('Veuillez fournir toutes les informations nécessaires : username, nom du produit, email, contact, prix, quantité et poids.');
     }
 
     try {
-        // Préparer les options de l'e-mail
         const mailOptions = {
             from: 'kaboreabwa2020@gmail.com',
             to: email,
@@ -133,13 +110,11 @@ app.post('/api/order', async (req, res) => {
             text: `Merci, ${username}, pour votre commande !\n\nVoici les détails :\n- Produit : ${nomproduit}\n- Prix total : ${price} FCFA\n- Quantité : ${quantity}\n- Poids total : ${weight} kg\n\nNous vous contacterons au numéro ${contact} pour valider la commande.\n\nNB : Les animaux subiront un contrôle de santé avec l'un de nos vétérinaires, et vous serez livré dans un délai de 24 heures maximum. Le paiement se fera à la livraison.`
         };
 
-        // Envoyer l'e-mail
         transporter.sendMail(mailOptions, (error, info) => {
             if (error) {
                 console.error('Erreur lors de l\'envoi de l\'e-mail :', error);
                 return res.status(500).send('Erreur lors de l\'envoi de l\'e-mail : ' + error.message);
             }
-
             console.log('E-mail envoyé : ' + info.response);
             res.status(200).send('Commande passée avec succès et e-mail envoyé !');
         });
@@ -159,7 +134,6 @@ app.get('/Visiteur', (req, res) => {
     res.sendFile(__dirname + '/public/Visiteur.html');
 });
 
-
 // Route pour récupérer les informations de l'utilisateur
 app.post('/api/getUserInfo', async (req, res) => {
     const userId = req.body.userId;
@@ -174,7 +148,6 @@ app.post('/api/getUserInfo', async (req, res) => {
 // Route pour envoyer les informations d'achat par email
 app.post('/api/send-email', async (req, res) => {
     const { content } = req.body;
-
     const mailOptions = {
         from: 'kaboreabwa2020@gmail.com',
         to: 'kaboreabwa2020@gmail.com',
@@ -187,7 +160,6 @@ app.post('/api/send-email', async (req, res) => {
             console.error('Erreur lors de l\'envoi de l\'e-mail :', error);
             return res.status(500).send('Erreur lors de l\'envoi de l\'e-mail.');
         }
-
         console.log('E-mail envoyé :', info.response);
         res.status(200).send('E-mail envoyé avec succès.');
     });
@@ -238,72 +210,23 @@ app.post('/api/messages/:id/replies', (req, res) => {
 
 // Routes pour les utilisateurs
 app.post('/api/users', (req, res) => {
-    const { username, password } = req.body; // Ne pas stocker le mot de passe en clair
-    const newUser = new User({ username, password });
+    const { username, email, contact } = req.body;
+    const newUser = new User({ username, email, contact });
     newUser.save()
-        .then(savedUser => res.status(201).json(savedUser))
+        .then(savedUser => res.json(savedUser))
         .catch(err => res.status(400).json(err));
 });
 
-app.get('/api/users', (req, res) => {
-    User.find()
-        .then(users => res.json(users))
-        .catch(err => res.status(400).json(err));
+// Configuration du serveur
+const PORT = process.env.PORT || 3002;
+server.listen(PORT, () => {
+    console.log(`Serveur en écoute sur le port ${PORT}`);
 });
 
-app.get('/api/users/:id', (req, res) => {
-    User.findById(req.params.id)
-        .then(user => {
-            if (!user) return res.status(404).send();
-            res.json(user);
-        })
-        .catch(err => res.status(400).json(err));
-});
-
-app.put('/api/users/:id', (req, res) => {
-    const { username, password } = req.body; // Ne pas stocker le mot de passe en clair
-    User.findByIdAndUpdate(req.params.id, { username, password }, { new: true })
-        .then(updatedUser => res.json(updatedUser))
-        .catch(err => res.status(400).json(err));
-});
-
-app.delete('/api/users/:id', (req, res) => {
-    User.findByIdAndDelete(req.params.id)
-        .then(() => res.status(204).send())
-        .catch(err => res.status(400).json(err));
-});
-
-// Démarrer le serveur
-const PORT = process.env.PORT || 3002; // 3002 est un port par défaut
-app.listen(PORT, () => {
-    console.log(`Le serveur est en marche sur le port ${PORT}...`);
-});
-
-
-
-
-// Toujours dans server.js ou app.js
-const Annonce = require('./models/Annonce'); // Assure-toi que le chemin du modèle est correct
-
-// Route pour ajouter une annonce
-app.post('/api/annonces', async (req, res) => {
-    try {
-        const annonce = new Annonce(req.body);
-        await annonce.save(); // Sauvegarde l'annonce dans MongoDB
-        res.status(201).json({ message: 'Annonce ajoutée avec succès' });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Erreur lors de l\'ajout de l\'annonce' });
-    }
-});
-
-// Route pour récupérer toutes les annonces
-app.get('/api/annonces', async (req, res) => {
-    try {
-        const annonces = await Annonce.find(); // Récupère toutes les annonces depuis MongoDB
-        res.json(annonces);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Erreur lors du chargement des annonces' });
-    }
+// WebSocket pour les communications en temps réel
+io.on('connection', (socket) => {
+    console.log('Nouvel utilisateur connecté');
+    socket.on('disconnect', () => {
+        console.log('Utilisateur déconnecté');
+    });
 });
