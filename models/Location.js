015c89d32dc67bd239b59d1c214eb494b5a21aaf
Kabore Abdoul wahab <kaboreabwa2020@gmail.com>
2024-12-11 09:03:36 +0000

    Votre message de mise à jour

--- models/Location.js
+++ models/Location.js
@@ -2,7 +2,6 @@
 const mongoose = require('mongoose');
 
 const locationSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     email: { type: String, required: true },
     latitude: { type: Number, required: true },
     longitude: { type: Number, required: true },
