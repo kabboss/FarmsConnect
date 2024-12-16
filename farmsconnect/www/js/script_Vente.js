0746f5deb3d80f82183e6efb0481afe6a293106f
Kabore Abdoul wahab <kaboreabwa2020@gmail.com>
2024-12-16 04:19:07 +0000

    Votre message de mise à jour

--- farmsconnect/www/js/script_Vente.js
+++ farmsconnect/www/js/script_Vente.js
@@ -52,7 +52,9 @@ document.getElementById('vente-form').addEventListener('submit', function(e) {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(animal) // Assurez-vous que 'animal' est bien formé
+            
         })
+        
         .then(response => {
             // Vérifiez si la réponse est réussie
             if (!response.ok) {
@@ -66,7 +68,6 @@ document.getElementById('vente-form').addEventListener('submit', function(e) {
         })
         .then(data => {
             // Affichez le message de succès
-            showAlert(data.message);
         })
         
     });
