6af9ddbefabaf4948ed23815256e33ecb88462f4
Kabore Abdoul wahab <kaboreabwa2020@gmail.com>
2024-12-23 16:11:47 +0000

    Votre message de mise à jour

--- public/index.js
+++ public/index.js
@@ -1,6 +1,9 @@
-function openInBrowser(url) {
-    var ref = cordova.InAppBrowser.open(url, '_system'); // Ouvre le lien dans le navigateur externe (comme Chrome)
-}
+document.addEventListener('deviceready', function() {
+    // Fonction pour ouvrir le lien dans le navigateur externe
+    function openInBrowser(url) {
+        cordova.InAppBrowser.open(url, '_system'); // Ouvre le lien dans le navigateur externe (comme Chrome)
+    }
 
-// Exemple d'utilisation de la fonction
-openInBrowser("https://farmsconnect-b084ddb02391.herokuapp.com");
+    // Exemple d'utilisation de la fonction
+    openInBrowser("https://farmsconnect-b084ddb02391.herokuapp.com");
+}, false);
